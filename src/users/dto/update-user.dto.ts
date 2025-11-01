fdca717193388496cfc15730c9a45e28bad91048
WarachotInkun <warachot.iq@gmail.com>
2025-11-01 18:35:46 +0700

    feat: add image upload functionality for places and update user DTO with additional fields

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,40 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsDate, IsEnum, IsOptional } from 'class-validator';
+import { Role } from './create-user.dto';
+export class UpdateUserDto {
+      @ApiPropertyOptional()
+      fname?: string;
+    
+      @ApiPropertyOptional()
+      lname?: string;
+    
+      @ApiPropertyOptional()
+      username?: string;
+    
+      @ApiPropertyOptional()
+      @IsDate()
+      birthDate?: Date;
+    
+      @ApiPropertyOptional({ enum: Role })
+      @IsEnum(Role)
+      role?: Role;
+    
+      @ApiPropertyOptional()
+      gender?: string;
+    
+      @ApiPropertyOptional()
+      @IsOptional()
+      email?: string;
+    
+      @ApiPropertyOptional()
+      phone?: string;
+    
+      @ApiPropertyOptional()
+      status?: string;
+    
+      @ApiProperty({ type: 'string', format: 'binary', required: false })
+      profileImg?: any;
+}
 
